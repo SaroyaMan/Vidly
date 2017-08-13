b59303d1e25b8dbada396ecbe2825d14ede01bb6
Yoav Saroya <stankovic100@gmail.com>
2017-08-13 15:15:48 +0300

    Lecture 33 is DONE

--- Vidly-New/Models/Customer.cs
+++ Vidly-New/Models/Customer.cs
@@ -13,6 +13,7 @@ namespace Vidly.Models {
         [StringLength(255)]
         public string Name { get; set; }
 
+        
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
